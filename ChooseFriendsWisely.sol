90ccd5fd95e4472b85588de8400a70a9f8ab0adf
Manish kumar <91535577+luckyankii@users.noreply.github.com>
2023-12-08 23:15:50 +0530

    Update ChooseFriendsWisely.sol

--- ChooseFriendsWisely.sol
+++ ChooseFriendsWisely.sol
@@ -1,1 +1,31 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ChooseFriendsWisely is ERC20, Ownable {
+    mapping(address => bool) private ItemsRedeemed;
+    uint public constant Loot = 1000 * 10;
+
+    constructor() ERC20("ChooseFriendsWisely", "CFW") {
+        _mint(msg.sender, Loot);
+    }
+
+    function mint(address acnt, uint amnt) public onlyOwner {
+        _mint(acnt, amnt);
+    }
+
+    function redeemItem() public {
+        require(balanceOf(msg.sender) >= Loot, "Balance is insufficient");
+        require(!ItemsRedeemed[msg.sender], "Items are redeemed already");
+
+        ItemsRedeemed[msg.sender] = true;
+        _burn(msg.sender, Loot);
+    }
+
+    function burn(uint amnt) public {
+        _burn(msg.sender, amnt);
+    }
+}
