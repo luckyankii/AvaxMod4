40aa6a0e99adbea0e86d4eaa84077dc5cc9e9748
Manish kumar <91535577+luckyankii@users.noreply.github.com>
2023-12-12 12:17:53 +0530

    Update ChooseFriendsWisely.sol

--- ChooseFriendsWisely.sol
+++ ChooseFriendsWisely.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ChooseFriendsWisely is ERC20, Ownable {
+contract ChooseFriendsWisely is ERC20, Ownable(msg.sender)  {
     mapping(address => bool) private ItemsRedeemed;
-    uint public constant Loot = 1000 * 10;
+    uint public constant bal = 1000;
 
     constructor() ERC20("ChooseFriendsWisely", "CFW") {
-        _mint(msg.sender, Loot);
+        _mint(msg.sender, bal);
     }
 
     function mint(address acnt, uint amnt) public onlyOwner {
@@ -18,11 +18,11 @@ contract ChooseFriendsWisely is ERC20, Ownable {
     }
 
     function redeemItem() public {
-        require(balanceOf(msg.sender) >= Loot, "Balance is insufficient");
+        require(balanceOf(msg.sender) >= bal, "Balance is insufficient");
         require(!ItemsRedeemed[msg.sender], "Items are redeemed already");
 
         ItemsRedeemed[msg.sender] = true;
-        _burn(msg.sender, Loot);
+        _burn(msg.sender, bal);
     }
 
     function burn(uint amnt) public {
